617a2ee9a627d127bffddde41032a8dd8189a757
Meena Alfons <meena.kerolos@gmail.com>
2019-05-08 21:56:41 +0200

    Add Trainer

---
+++ Trainer.py
@@ -0,0 +1,400 @@
+from __future__ import absolute_import, division, print_function
+
+import logging
+import os
+import random
+import time
+
+import numpy as np
+import torch
+from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
+                              TensorDataset)
+from torch.utils.data.distributed import DistributedSampler
+from tqdm import tqdm, trange
+
+from torch.nn import CrossEntropyLoss, MSELoss
+
+from pytorch_pretrained_bert.file_utils import PYTORCH_PRETRAINED_BERT_CACHE, WEIGHTS_NAME, CONFIG_NAME
+from pytorch_pretrained_bert.modeling import BertForSequenceClassification, BertConfig
+from pytorch_pretrained_bert.tokenization import BertTokenizer
+from pytorch_pretrained_bert.optimization import BertAdam, WarmupLinearSchedule #, warmup_linear
+
+from utils import compute_metrics, convert_examples_to_features
+
+class Trainer:
+
+    def __init__(self, args):
+        self.logger = logger = logging.getLogger(__name__)
+        self.args = args
+
+    def save_model(self, model, tokenizer, output_dir):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # Save a trained model, configuration and tokenizer
+        model_to_save = self.model.module if hasattr(self.model, 'module') else self.model  # Only save the model it-self
+
+        # If we save using the predefined names, we can load using `from_pretrained`
+        output_model_file = os.path.join(output_dir, WEIGHTS_NAME)
+        output_config_file = os.path.join(output_dir, CONFIG_NAME)
+
+        torch.save(model_to_save.state_dict(), output_model_file)
+        model_to_save.config.to_json_file(output_config_file)
+        self.tokenizer.save_vocabulary(output_dir)
+
+
+    def evaluate(self, print_preds):
+        logger.info("***** Running evaluation *****")
+        logger.info("  Num examples = %d", len(self.eval_examples))
+        logger.info("  Batch size = %d", self.args.eval_batch_size)
+
+        self.model.eval()
+        eval_loss = 0
+        nb_eval_steps = 0
+        preds = []
+
+        for input_ids, input_mask, segment_ids, label_ids in tqdm(self.eval_dataloader, desc="Evaluating"):
+            input_ids = input_ids.to(self.device)
+            input_mask = input_mask.to(self.device)
+            segment_ids = segment_ids.to(self.device)
+            label_ids = label_ids.to(self.device)
+
+            with torch.no_grad():
+                logits = self.model(input_ids, segment_ids, input_mask, labels=None)
+
+            # create eval loss and other metric required by the task
+            if self.output_mode == "classification":
+                loss_fct = CrossEntropyLoss()
+                tmp_eval_loss = loss_fct(logits.view(-1, num_labels), label_ids.view(-1))
+            elif self.output_mode == "regression":
+                loss_fct = MSELoss()
+                tmp_eval_loss = loss_fct(logits.view(-1), label_ids.view(-1))
+
+            eval_loss += tmp_eval_loss.mean().item()
+            nb_eval_steps += 1
+            logits = logits.detach().cpu().numpy()
+            
+            if len(preds) == 0:
+                preds.append(logits)
+            else:
+                preds[0] = np.append(
+                    preds[0], logits, axis=0)
+
+        eval_loss = eval_loss / nb_eval_steps
+        preds = preds[0]
+        if self.output_mode == "classification":
+            preds = np.argmax(preds, axis=1)
+        elif self.output_mode == "regression":
+            preds = np.squeeze(preds)
+        
+        eval_all_label_ids_numpy = self.eval_all_label_ids.numpy()
+        result = compute_metrics("many_metrics", preds, eval_all_label_ids_numpy)
+
+        if print_preds:
+            for i, pred in enumerate(preds):
+                print("i = {}\t\tPredicted = {}\t\tActual = {}".format(i, pred, eval_all_label_ids_numpy[i]))
+
+        result['eval_loss'] = eval_loss
+        return result
+
+    def save_result(self, result, epoch_output_dir):
+        output_eval_file = os.path.join(epoch_output_dir, "eval_results.txt")
+        with open(output_eval_file, "w") as writer:
+            logger.info("***** Eval results *****")
+            for key in sorted(result.keys()):
+                logger.info("  %s = %s", key, str(result[key]))
+                writer.write("%s = %s\n" % (key, str(result[key])))
+
+    def preprare_distant_debugging(self):
+        if self.args.server_ip and self.args.server_port:
+            # Distant debugging - see https://code.visualstudio.com/docs/python/debugging#_attach-to-a-local-script
+            import ptvsd
+            print("Waiting for debugger attach")
+            ptvsd.enable_attach(address=(self.args.server_ip, self.args.server_port), redirect_output=True)
+            ptvsd.wait_for_attach()
+            
+    def prepare_device(self):
+        if self.args.local_rank == -1 or self.args.no_cuda:
+            self.device = torch.device("cuda" if torch.cuda.is_available() and not self.args.no_cuda else "cpu")
+            self.n_gpu = torch.cuda.device_count()
+        else:
+            torch.cuda.set_device(self.args.local_rank)
+            self.device = torch.device("cuda", self.args.local_rank)
+            self.n_gpu = 1
+            # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+            torch.distributed.init_process_group(backend='nccl')
+
+    def prepare_logging(self):
+        logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                            datefmt = '%m/%d/%Y %H:%M:%S',
+                            level = logging.INFO if self.args.local_rank in [-1, 0] else logging.WARN)
+
+        logger.info("device: {} n_gpu: {}, distributed training: {}, 16-bits training: {}".format(
+            self.device, self.n_gpu, bool(self.args.local_rank != -1), self.args.fp16))
+
+    def seed(self):
+        random.seed(self.args.seed)
+        np.random.seed(self.args.seed)
+        torch.manual_seed(self.args.seed)
+        if self.n_gpu > 0:
+            torch.cuda.manual_seed_all(self.args.seed)
+
+    def prepare_model(self):
+        if self.args.resume_dir:
+            self.model = BertForSequenceClassification.from_pretrained(self.args.resume_dir, num_labels=self.num_labels)
+            self.tokenizer = BertTokenizer.from_pretrained(self.args.resume_dir, do_lower_case=self.args.do_lower_case)
+        else:
+            self.tokenizer = BertTokenizer.from_pretrained(self.args.bert_model, do_lower_case=self.args.do_lower_case)
+            cache_dir = self.args.cache_dir if self.args.cache_dir else os.path.join(str(PYTORCH_PRETRAINED_BERT_CACHE), 'distributed_{}'.format(self.args.local_rank))
+            self.model = BertForSequenceClassification.from_pretrained(self.args.bert_model,
+                    cache_dir=cache_dir,
+                    num_labels=self.num_labels)
+
+        if self.args.fp16:
+            self.model.half()
+            
+        self.model.to(self.device)
+        
+        if self.args.local_rank != -1:
+            try:
+                from apex.parallel import DistributedDataParallel as DDP
+            except ImportError:
+                raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use distributed and fp16 training.")
+
+            self.model = DDP(self.model)
+        elif self.n_gpu > 1:
+            self.model = torch.nn.DataParallel(self.model)
+
+    def prepare_optimizer(self):
+        param_optimizer = list(self.model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            ]
+        if self.args.fp16:
+            try:
+                from apex.optimizers import FP16_Optimizer
+                from apex.optimizers import FusedAdam
+            except ImportError:
+                raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use distributed and fp16 training.")
+
+            self.optimizer = FusedAdam(optimizer_grouped_parameters,
+                                lr=self.args.learning_rate,
+                                bias_correction=False,
+                                max_grad_norm=1.0)
+            self.warmup_schedule = WarmupLinearSchedule(warmup=self.args.warmup_proportion, t_total=self.num_train_optimization_steps)
+            
+            if self.args.loss_scale == 0:
+                self.optimizer = FP16_Optimizer(optimizer, dynamic_loss_scale=True)
+            else:
+                self.optimizer = FP16_Optimizer(optimizer, static_loss_scale=self.args.loss_scale)
+
+        else:
+            self.optimizer = BertAdam(optimizer_grouped_parameters,
+                                lr=self.args.learning_rate,
+                                warmup=self.args.warmup_proportion,
+                                t_total=self.num_train_optimization_steps)
+
+    def prepare_train_examples(self):
+        self.train_examples = self.processor.get_train_examples(self.args.data_dir)
+        self.num_train_optimization_steps = int(
+            len(train_examples) / self.args.train_batch_size / self.args.gradient_accumulation_steps) * self.args.num_train_epochs
+        if self.args.local_rank != -1:
+            self.num_train_optimization_steps = num_train_optimization_steps // torch.distributed.get_world_size()
+        self.label_weights = self.processor.get_weights()
+        print("label_weights = {}".format(label_weights))
+    
+        train_features = convert_examples_to_features(
+            train_examples, self.label_list, self.args.max_seq_length, self.tokenizer, self.output_mode)
+        all_input_ids = torch.tensor([f.input_ids for f in train_features], dtype=torch.long)
+        all_input_mask = torch.tensor([f.input_mask for f in train_features], dtype=torch.long)
+        all_segment_ids = torch.tensor([f.segment_ids for f in train_features], dtype=torch.long)
+
+        if self.output_mode == "classification":
+            all_label_ids = torch.tensor([f.label_id for f in train_features], dtype=torch.long)
+        elif self.output_mode == "regression":
+            all_label_ids = torch.tensor([f.label_id for f in train_features], dtype=torch.float)
+
+        train_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+        if self.args.local_rank == -1:
+            train_sampler = RandomSampler(train_data)
+        else:
+            train_sampler = DistributedSampler(train_data)
+        self.train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=self.args.train_batch_size)
+
+    def preprare_eval_examples(self):
+        self.eval_examples = self.processor.get_dev_examples(self.args.data_dir)
+        self.eval_features = convert_examples_to_features(
+            eval_examples, self.label_list, self.args.max_seq_length, self.tokenizer, self.output_mode)
+        all_input_ids = torch.tensor([f.input_ids for f in self.eval_features], dtype=torch.long)
+        all_input_mask = torch.tensor([f.input_mask for f in self.eval_features], dtype=torch.long)
+        all_segment_ids = torch.tensor([f.segment_ids for f in self.eval_features], dtype=torch.long)
+
+        if self.output_mode == "classification":
+            all_label_ids = torch.tensor([f.label_id for f in self.eval_features], dtype=torch.long)
+        elif self.output_mode == "regression":
+            all_label_ids = torch.tensor([f.label_id for f in self.eval_features], dtype=torch.float)
+
+        eval_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+        # Run prediction for full data
+        eval_sampler = SequentialSampler(eval_data)
+        self.eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=self.args.eval_batch_size)
+
+    def train(self):
+        logger.info("***** Running training *****")
+        logger.info("  Num examples = %d", len(train_examples))
+        logger.info("  Batch size = %d", self.args.train_batch_size)
+        logger.info("  Num steps = %d", num_train_optimization_steps)
+
+        
+        if self.output_mode == "classification":
+            print("label_weights = {}".format(self.label_weights))
+            label_weights = torch.tensor(self.label_weights).float().to(self.device)
+            loss_fct = CrossEntropyLoss(weight=label_weights)
+        elif self.output_mode == "regression":
+            loss_fct = MSELoss()
+
+        self.model.train()
+        
+        global_step = 0
+        nb_tr_steps = 0
+        tr_loss = 0
+        for epoch in trange(int(self.args.num_train_epochs), desc="Epoch"):
+            self.model.train()
+
+            if epoch == self.args.resume_epochs - 1:
+                time.sleep(1)
+                tqdm.write("\nEpoch {} previously done\n".format(epoch))
+
+            if epoch < self.args.resume_epochs:    
+                continue
+            elif epoch == self.args.resume_epochs:
+                tqdm.write("\nResuming Epoch {}.\n".format(epoch))
+
+            tr_loss = 0
+            nb_tr_examples, nb_tr_steps = 0, 0
+#             start_time = time.time()
+            for step, batch in enumerate(tqdm(self.train_dataloader, desc="Iteration")):
+#                 print("Reading Batch in {}".format(time.time() - start_time))
+
+                if epoch == self.args.resume_epochs and step == self.args.resume_steps - 1:
+                    time.sleep(1)
+                    tqdm.write("\nStep {} of epoch {} previously done\n".format(step, epoch))
+
+                if epoch == self.args.resume_epochs:
+                    if step < self.args.resume_steps:
+                        continue
+                    elif step == self.args.resume_steps:
+                        tqdm.write("\nResuming step {} from epoch {}.".format(step, epoch))
+
+#                 start_time = time.time()
+                batch = tuple(t.to(self.device) for t in batch)
+                input_ids, input_mask, segment_ids, label_ids = batch
+#                 print("Preparing Batch in {}".format(time.time() - start_time))
+
+                # define a new function to compute loss values for both output_modes
+#                 start_time = time.time()
+                logits = self.model(input_ids, segment_ids, input_mask, labels=None)
+#                 print("Execute model in {}".format(time.time() - start_time))
+
+#                 start_time = time.time()
+                if self.output_mode == "classification":
+                    loss = loss_fct(logits.view(-1, num_labels), label_ids.view(-1))
+                elif self.output_mode == "regression":
+                    loss = loss_fct(logits.view(-1), label_ids.view(-1))
+
+                if self.n_gpu > 1:
+                    loss = loss.mean() # mean() to average on multi-gpu.
+                if self.args.gradient_accumulation_steps > 1:
+                    loss = loss / self.args.gradient_accumulation_steps
+#                 print("Calculate loss in {}".format(time.time() - start_time))
+
+#                 start_time = time.time()
+                if self.args.fp16:
+                    self.optimizer.backward(loss)
+                else:
+                    loss.backward()
+#                 print("Backword loss in {}".format(time.time() - start_time))
+
+#                 start_time = time.time()
+                tr_loss += loss.item()
+                nb_tr_examples += input_ids.size(0)
+                nb_tr_steps += 1
+                if (step + 1) % self.args.gradient_accumulation_steps == 0:
+                    if self.args.fp16:
+                        # modify learning rate with special warm up BERT uses
+                        # if self.args.fp16 is False, BertAdam is used that handles this automatically
+                        lr_this_step = self.args.learning_rate * warmup_schedule.get_lr(global_step/num_train_optimization_steps)
+#                         lr_this_step = self.args.learning_rate * warmup_linear(global_step/num_train_optimization_steps, self.args.warmup_proportion)
+                        for param_group in self.optimizer.param_groups:
+                            param_group['lr'] = lr_this_step
+                    self.optimizer.step()
+                    self.optimizer.zero_grad()
+                    global_step += 1
+#                 print("Optimize in {}".format(time.time() - start_time))
+
+                if (step+1) % self.args.save_model_steps == 0:
+                    # Save model
+                    # ...
+                    step_output_dir = os.path.join(self.args.output_dir, "epoch_{}_step_{}".format(epoch, step))
+                    self.save_model(self.model, self.tokenizer, step_output_dir)
+
+#                 start_time = time.time()
+
+            # Save model at the end of Epoch
+            # ...
+            epoch_output_dir = os.path.join(self.args.output_dir, "epoch_{}".format(epoch))
+            self.save_model(self.model, self.tokenizer, epoch_output_dir)
+            
+            # Evaluate Epoch
+            result = self.evaluate(False)
+            result['tr_loss'] = tr_loss/nb_tr_steps
+            self.save_result(result, epoch_output_dir)
+            
+        
+    def execute(self):
+        self.preprare_distant_debugging()
+
+        self.prepare_device()
+
+        self.prepare_logging()
+
+        if self.args.gradient_accumulation_steps < 1:
+            raise ValueError("Invalid gradient_accumulation_steps parameter: {}, should be >= 1".format(
+                                self.args.gradient_accumulation_steps))
+
+        self.args.train_batch_size = self.args.train_batch_size // self.args.gradient_accumulation_steps
+
+        self.seed()
+
+        if not self.args.do_train and not self.args.do_eval:
+            raise ValueError("At least one of `do_train` or `do_eval` must be True.")
+
+        if os.path.exists(self.args.output_dir) and os.listdir(self.args.output_dir) and self.args.do_train:
+            raise ValueError("Output directory ({}) already exists and is not empty.".format(self.args.output_dir))
+        if not os.path.exists(self.args.output_dir):
+            os.makedirs(self.args.output_dir)
+
+        self.processor = self.args.processor()
+        self.output_mode = self.args.output_mode
+
+        self.label_list = processor.get_labels()
+        self.num_labels = len(label_list)
+
+        self.prepare_train_examples()
+
+        self.prepare_model()
+
+        self.prepare_optimizer()
+
+        self.preprare_eval_examples()
+        
+        if self.args.do_train:
+            self.train()
+
+        if self.args.do_eval and (self.args.local_rank == -1 or torch.distributed.get_rank() == 0):
+            result = self.evaluate(True)
+            
+            output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
+            self.save_result(result, output_eval_file)
---
+++ processor.py
@@ -0,0 +1,114 @@
+import os
+import json
+
+import numpy as np
+
+from utils import InputExample
+
+class EmotionX2019Processor(DataProcessor):
+    """Processor for the RTE data set (GLUE version)."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(os.path.join(data_dir, "train_1.json"), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(os.path.join(data_dir, "dev.json"), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ["non-neutral", "joy", "sadness", "surprise", "anger", "fear", "disgust", "neutral"]
+      
+    def get_weights(self):
+        labels = self.get_labels()
+        weights = []
+        total = 0
+        for i, label in enumerate(labels):
+            total += self.stats[label]
+            weights.append(self.stats[label])
+        
+        print("weights list = {}".format(weights))
+        weights = np.array(weights, np.double)
+        weights = total - weights
+        weights = weights / max(weights)
+        print("Final weights = {}".format(weights))
+        return weights
+      
+    def _create_examples(self, file_name, set_type):
+        """Creates examples for the training and dev sets."""   
+        print("Creating Examples ...................")
+        stats = {}
+        labels = self.get_labels()
+        for label in labels:
+            stats[label] = 0
+        
+        examples = []     
+        with open(file_name) as file:
+            source = json.load(file)
+            for (i, diag) in enumerate(source):
+                for (j, item) in enumerate(diag):
+                    guid = "{}-{}-{}".format(set_type, i, j)
+                    example = self._create_example_of_item(guid, diag, j);
+                    examples.append(example)
+                    stats[example.label] += 1
+                    
+        print("stats = {}".format(stats))
+        self.stats = stats
+        return examples
+      
+    def _create_example_of_item(self, guid, diag, i):
+        raise NotImplementedError()
+      
+
+class EmotionX2019_OneSequence_Processor(EmotionX2019Processor):
+    """Processor for the RTE data set (GLUE version)."""
+
+    def _create_example_of_item(self, guid, diag, i):
+        item = diag[i]
+        text_a = item["utterance"]
+        label = item["emotion"]
+        return InputExample(guid=guid, text_a=text_a, label=label)
+
+
+class EmotionX2019_TwoSequence_Processor(EmotionX2019Processor):
+    """Processor for the RTE data set (GLUE version)."""
+
+    def _create_example_of_item(self, guid, diag, i):
+        item = diag[i]
+        text_a = item["utterance"]
+        label = item["emotion"]
+        text_b=None
+        j = i+1
+        while j < len(diag) :
+            if(diag[j]["speaker"] == item["speaker"]):
+                text_b = diag[j]["utterance"]
+                break
+            j = j + 1
+#         print("text_a={}".format(text_a))
+#         print("text_b={}".format(text_b))
+        return InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
+
+class EmotionX2019_PrevSameSpeaker_Processor(EmotionX2019Processor):
+    """Processor for the RTE data set (GLUE version)."""
+
+    def _prevSameSpeakerItem(self, diag, i):
+        origItem = diag[i]
+        j = i-1
+        while j>=0:
+            currentItem = diag[j]
+            if currentItem["speaker"] == origItem["speaker"]:
+                return currentItem
+            j = j - 1
+        return None
+    
+    def _create_example_of_item(self, guid, diag, i):
+        item = diag[i]
+        text_b = item["utterance"]
+        label = item["emotion"]
+        text_a=''
+        
+        prevItem = self._prevSameSpeakerItem(diag, i)
+        if prevItem:
+            text_a = prevItem["utterance"]
+        return InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
