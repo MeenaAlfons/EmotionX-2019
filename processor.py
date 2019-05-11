86ab59fb902c91561825d2005d60eda6bc713c92
Meena Alfons <meena.kerolos@gmail.com>
2019-05-11 13:11:35 +0200

    Processor changes

--- processor.py
+++ processor.py
@@ -10,7 +10,7 @@ class EmotionX2019Processor(DataProcessor):
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(os.path.join(data_dir, "train_1.json"), "train")
+        return self._create_examples(os.path.join(data_dir, "train.json"), "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
@@ -34,6 +34,9 @@ class EmotionX2019Processor(DataProcessor):
         weights = weights / max(weights)
         print("Final weights = {}".format(weights))
         return weights
+    
+    def is_pair(self):
+        return False
       
     def _create_examples(self, file_name, set_type):
         """Creates examples for the training and dev sets."""   
@@ -59,7 +62,7 @@ class EmotionX2019Processor(DataProcessor):
       
     def _create_example_of_item(self, guid, diag, i):
         raise NotImplementedError()
-      
+
 
 class EmotionX2019_OneSequence_Processor(EmotionX2019Processor):
     """Processor for the RTE data set (GLUE version)."""
@@ -74,6 +77,27 @@ class EmotionX2019_OneSequence_Processor(EmotionX2019Processor):
 class EmotionX2019_TwoSequence_Processor(EmotionX2019Processor):
     """Processor for the RTE data set (GLUE version)."""
 
+    def is_pair(self):
+        return True
+    
+    def truncate_seq_pair(self, tokens_a, tokens_b, max_length):
+        pass
+        
+
+
+class EmotionX2019_NextSameSpeaker_Processor(EmotionX2019_TwoSequence_Processor):
+    
+    def truncate_seq_pair(self, tokens_a, tokens_b, max_length):
+        while True:
+            total_length = len(tokens_a) + len(tokens_b)
+            if total_length <= max_length:
+                break
+            if len(tokens_b) > 0:
+                tokens_b.pop()
+            else:
+                tokens_a.pop()
+            
+        
     def _create_example_of_item(self, guid, diag, i):
         item = diag[i]
         text_a = item["utterance"]
@@ -89,9 +113,20 @@ class EmotionX2019_TwoSequence_Processor(EmotionX2019Processor):
 #         print("text_b={}".format(text_b))
         return InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
 
-class EmotionX2019_PrevSameSpeaker_Processor(EmotionX2019Processor):
+class EmotionX2019_PrevSameSpeaker_Processor(EmotionX2019_TwoSequence_Processor):
     """Processor for the RTE data set (GLUE version)."""
 
+    def truncate_seq_pair(self, tokens_a, tokens_b, max_length):
+        while True:
+            total_length = len(tokens_a) + len(tokens_b)
+            if total_length <= max_length:
+                break
+                
+            if len(tokens_a) > 0:
+                tokens_a.pop()
+            else:
+                tokens_b.pop()
+            
     def _prevSameSpeakerItem(self, diag, i):
         origItem = diag[i]
         j = i-1
@@ -109,6 +144,41 @@ class EmotionX2019_PrevSameSpeaker_Processor(EmotionX2019Processor):
         text_a=''
         
         prevItem = self._prevSameSpeakerItem(diag, i)
+        if prevItem:
+            text_a = prevItem["utterance"]
+        return InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
+
+class EmotionX2019_PrevDiffSpeaker_Processor(EmotionX2019_TwoSequence_Processor):
+    """Processor for the RTE data set (GLUE version)."""
+
+    def truncate_seq_pair(self, tokens_a, tokens_b, max_length):
+        while True:
+            total_length = len(tokens_a) + len(tokens_b)
+            if total_length <= max_length:
+                break
+                
+            if len(tokens_a) > 0:
+                tokens_a.pop()
+            else:
+                tokens_b.pop()
+            
+    def _prevDiffSpeakerItem(self, diag, i):
+        origItem = diag[i]
+        j = i-1
+        while j>=0:
+            currentItem = diag[j]
+            if currentItem["speaker"] != origItem["speaker"]:
+                return currentItem
+            j = j - 1
+        return None
+    
+    def _create_example_of_item(self, guid, diag, i):
+        item = diag[i]
+        text_b = item["utterance"]
+        label = item["emotion"]
+        text_a=''
+        
+        prevItem = self._prevDiffSpeakerItem(diag, i)
         if prevItem:
             text_a = prevItem["utterance"]
         return InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
